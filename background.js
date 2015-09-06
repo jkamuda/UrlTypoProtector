f766376a63f47df859dbcda17fa43d39000de1ff
Jason Kamuda <jkamuda@gmail.com>
2015-09-06 02:17:34 -0400

    let's standardize on tabs

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
+  function(details) {
     var re = /yahoo\.com/;
     if (details.url.match(re)) {
       return {redirectUrl: chrome.extension.getURL("redirectInfo.html")};
     }
-	},
-	{
+  },
+  {
     urls: ["<all_urls>"],
     types: ['main_frame']
   },
-	["blocking"]
+  ["blocking"]
 );
