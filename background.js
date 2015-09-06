d7bac0cc8eb0c0e730bbfae5af07945027b1875a
Jason Kamuda <jkamuda@gmail.com>
2015-09-06 02:13:14 -0400

    initial revision that redirects yahoo.com domains to extension page

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+    var re = /yahoo\.com/;
+    if (details.url.match(re)) {
+      return {redirectUrl: chrome.extension.getURL("redirectInfo.html")};
+    }
+	},
+	{
+    urls: ["<all_urls>"],
+    types: ['main_frame']
+  },
+	["blocking"]
+);
