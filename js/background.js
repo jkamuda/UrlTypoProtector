1ae60be6774fdac8dad64e265f4725a0d97926a9
Jason Kamuda <jkamuda@gmail.com>
2015-12-27 22:25:13 -0500

    make that callout

--- js/background.js
+++ js/background.js
@@ -4,7 +4,10 @@ chrome.webRequest.onBeforeRequest.addListener(
     if (isGoogle(domain)) {
       return;
     }
-    console.log(domain);
+    //console.log(domain);
+    if (!callout(domain)) {
+      return {redirectUrl: chrome.extension.getURL("html/redirectInfo.html")};
+    }
     /*if (details.url.match(re)) {
       return {redirectUrl: chrome.extension.getURL("html/redirectInfo.html")};
     }*/
@@ -15,6 +18,17 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   ["blocking"]
 );
+
+function callout(domain) {
+  var url = "http://localhost:8080/validate/" + domain;
+  var client = new XMLHttpRequest();
+  client.open("GET", url, false);
+  client.setRequestHeader("Content-Type", "text/plain");
+  client.send();
+  console.log(client.status);
+  console.log(client);
+  return client.status === 200;
+}
 
 function stripDomain(fullUrl) {
   return fullUrl
