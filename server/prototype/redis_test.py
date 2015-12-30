8daf5c59dee7f407e603f2783391508cb1df061a
Jason Kamuda <jkamuda@gmail.com>
2015-12-29 21:08:42 -0500

    cleaned up permutation load tools

--- server/prototype/redis_test.py
+++ server/prototype/redis_test.py
@@ -1,19 +1,22 @@
 import redis
 import json
 
-import permutate
+import permutation_tools
 
 pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
 
-def fill(word):
+def load_word_permutations(word):
   r_server = redis.StrictRedis(connection_pool=pool)
-  correction = {}
-  correction['correction'] = word
-  correctionJson = json.dumps(correction)
 
-  for permutation in permutate.get_permutations(word):
-    print permutation
-    r_server.set(permutation, correctionJson)
+  correction_info = {}
+  correction_info['correction'] = word
+  correction_info_json = json.dumps(correction_info)
+
+  word_permutations = permutation_tools.get_permutations(word)
+  for permutation in word_permutations:
+    r_server.set(permutation, correction_info_json)
+
+  print 'loaded {0} permutations for {1}'.format(len(word_permutations), word)
 
 if __name__ == "__main__":
-  fill("youtube")
+  load_word_permutations("youtube")
